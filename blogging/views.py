36a716cc025b9c9c7917eb9b9f006cce8ed586f7
Josh Addington <josh.addington@gmail.com>
2015-02-26 16:07:24 -0500

    testing colors

--- blogging/views.py
+++ blogging/views.py
@@ -3,6 +3,9 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
+def about(request):
+        return render(request, 'about.html')
+
 def post_list(request):
         posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
         for line in posts:
