8c27e91e2f9cc0192816db87c28b2cc728390e8d
Josh Addington <josh.addington@gmail.com>
2015-03-04 17:18:32 -0500

    change button class to mybutton, to avoid conflict with CartoDB css

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
 from .models import Post, Comment
 
-# Register your models here.
-admin.site.register(Post)
-admin.site.register(Comment)
+
+class CommentInline(admin.TabularInline):
+        model = Comment
+        extra = 3
+
+
+class PostAdmin(admin.ModelAdmin):
+        fieldsets = [
+                (None, {'fields': ['author', 'title', 'project', 'text']}),
+                ('Date Information', {'fields': ['created_date', 'published_date']}),
+                ('Comments', {'fields': ['post.comment_set']}),
+        ]
+
+        inlines = [CommentInline]
+        list_display = ('title', 'project', 'created_date', 'published_date')
+
+
+class CommentAdmin(admin.ModelAdmin):
+        fieldsets = [
+                (None, {'fields': ['author', 'post', 'text', 'posted_date']}),
+        ]
+
+        list_display = ('post', 'author', 'text')
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
