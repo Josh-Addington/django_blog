82594f4fc1eca56408725e6418101504b7762377
Josh Addington <josh.addington@gmail.com>
2015-03-05 23:00:07 -0500

    Fix #3, ran 'python manage.py migrate blogging zero' to undo all migrations, cleared all migrate files for blogging app, and then migrated blogging app. This allowed the ManyToManyField to migrate.

--- blogging/migrations/0001_initial.py
+++ blogging/migrations/0001_initial.py
@@ -9,21 +9,42 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('projects', '0009_auto_20150305_1311'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Post',
+            name='Comment',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('title', models.CharField(max_length=200)),
+                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                ('author', models.CharField(max_length=25)),
                 ('text', models.TextField()),
                 ('posted_date', models.DateTimeField(default=django.utils.timezone.now)),
-                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
             options={
             },
             bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                ('title', models.CharField(max_length=200)),
+                ('text', models.TextField()),
+                ('created_date', models.DateTimeField(default=django.utils.timezone.now)),
+                ('published_date', models.DateTimeField(blank=True, null=True)),
+                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+                ('project', models.ManyToManyField(to='projects.Project')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='comment',
+            name='post',
+            field=models.ForeignKey(to='blogging.Post'),
+            preserve_default=True,
         ),
     ]
