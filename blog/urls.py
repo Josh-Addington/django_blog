eb2136e9f423ae2f89200f2a24c79446f5f0dfa7
Josh Addington <josh.addington@gmail.com>
2015-03-08 16:41:52 -0400

    normalize urls to use django url tag

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,4 @@
+from . import views
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
@@ -5,9 +6,11 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^', include('blogging.urls')),
+    url(r'^about/$', views.about),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+    url(r'^contact/$', views.contact),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, }),
     url(r'^projects/', include('projects.urls')),
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT, }),
