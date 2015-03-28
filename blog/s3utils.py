1fe971464f070e21f96633b23e7d2d4150f8cc16
joshaddington <josh.addington@gmail.com>
2015-03-28 00:06:42 -0400

    "AWS S3 test"

--- blog/s3utils.py
+++ blog/s3utils.py
@@ -1,4 +1,7 @@
 from storages.backends.s3boto import S3BotoStorage
 
-StaticRootS3BotoStorage = lambda: S3BotoStorage(location='static')
-MediaRootS3BotoStorage = lambda: S3BotoStorage(location='media')
+class StaticRootS3BotoStorage(S3BotoStorage):
+	location='static'
+
+class MediaRootS3BotoStorage(S3BotoStorage):
+	location='media'
