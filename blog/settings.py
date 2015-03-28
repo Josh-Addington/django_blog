5d7abe81c9b479739c93c7f8fc61d0c4123ccdc1
joshaddington <josh.addington@gmail.com>
2015-03-27 21:42:56 -0400

    test debug env var

--- blog/settings.py
+++ blog/settings.py
@@ -120,7 +120,7 @@ DATABASES['default'] = dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
-DEBUG = False
+DEBUG = os.environ['DEBUG']
 
 try:
     from .local_settings import *
--- projects/models.py
+++ projects/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 
-# Create your models here.
+# Model for keeping track of Projects
 class Project(models.Model):
         author = models.ForeignKey('auth.User')
         title = models.CharField(max_length=50)
