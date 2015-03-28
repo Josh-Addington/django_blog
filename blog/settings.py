bf2dbfa068d5917e8a48bd41b77319483f140e68
joshaddington <josh.addington@gmail.com>
2015-03-27 22:12:52 -0400

    test stage

--- blog/settings.py
+++ blog/settings.py
@@ -119,7 +119,7 @@ DATABASES['default'] = dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
-DEBUG = os.environ['DEBUG']
+DEBUG = os.environ['DJANGO_DEBUG']
 
 try:
     from .local_settings import *
