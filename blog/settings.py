fbf744225ec7416b65fdb59b52ddd32796ad58b3
joshaddington <josh.addington@gmail.com>
2015-03-27 22:24:07 -0400

    test staging

--- blog/settings.py
+++ blog/settings.py
@@ -9,8 +9,6 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 import os
 
-from storages.backends.s3boto import S3BotoStorage
-
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -39,9 +37,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-)
-
-INSTALLED_APPS += (
     'blogging',
     'projects',
     'storages',
