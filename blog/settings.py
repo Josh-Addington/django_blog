6afc6840886217d2f2adb273bc4b987f8459deb2
joshaddington <josh.addington@gmail.com>
2015-03-27 22:04:29 -0400

    Test stage

--- blog/settings.py
+++ blog/settings.py
@@ -7,7 +7,6 @@ https://docs.djangoproject.com/en/1.7/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
-import dj_database_url
 import os
 
 from storages.backends.s3boto import S3BotoStorage
@@ -114,7 +113,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-
+import dj_database_url
 DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
